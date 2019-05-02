e5206efd92ca8290cff4490c33069bf7201a0725
0u812 <0u812@users.noreply.github.com>
2019-05-02 13:14:17 -0700

    add .babelrc, got mocha working with npx mocha --require @babel/register

---
+++ test/index.js
@@ -0,0 +1,11 @@
+import { assert } from 'chai'
+
+describe('Basic Mocha String Test', function () {
+ it('should return number of charachters in a string', function () {
+        assert.equal("Hello".length, 4);
+    });
+
+ it('should return first charachter of the string', function () {
+        assert.equal("Hello".charAt(0), 'H');
+    });
+});
