96805fed0335e38eeff6e68488b8ee5ab22bfec7
0u812 <0u812@users.noreply.github.com>
2019-05-02 13:17:19 -0700

    pass 'tests'

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@ import { assert } from 'chai'
 
 describe('Basic Mocha String Test', function () {
  it('should return number of charachters in a string', function () {
-        assert.equal("Hello".length, 4);
+        assert.equal("Hello".length, 5);
     });
 
  it('should return first charachter of the string', function () {
