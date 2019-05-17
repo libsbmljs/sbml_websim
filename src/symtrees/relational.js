d5262e6069f82886e5b6885131905c2e070230e8
0u812 <0u812@users.noreply.github.com>
2019-05-17 14:36:17 -0700

    add some relationals

--- src/symtrees/relational.js
+++ src/symtrees/relational.js
@@ -1,6 +1,11 @@
 import { BinaryOperator } from './binaryop.js'
 
-export class RelationalEquals extends BinaryOperator {
+function bumpZero(v) {
+  if (v === 0)
+    return 0.0001
+}
+
+export class RelationalEqual extends BinaryOperator {
   constructor(lhs, rhs) {
     super(lhs, rhs)
   }
@@ -8,5 +13,39 @@ export class RelationalEquals extends BinaryOperator {
   evaluate(evaluator, initial=false, conc=true, bvars=null) {
     return this.lhs.evaluate(evaluator, initial, conc, bvars) ===
            this.rhs.evaluate(evaluator, initial, conc, bvars) ? 1 : -1
+  }
+}
+
+export class RelationalNotEqual extends BinaryOperator {
+  constructor(lhs, rhs) {
+    super(lhs, rhs)
+  }
+
+  evaluate(evaluator, initial=false, conc=true, bvars=null) {
+    return this.lhs.evaluate(evaluator, initial, conc, bvars) !==
+           this.rhs.evaluate(evaluator, initial, conc, bvars) ? 1 : -1
+  }
+}
+
+export class RelationalLT extends BinaryOperator {
+  constructor(lhs, rhs) {
+    super(lhs, rhs)
+  }
+
+  evaluate(evaluator, initial=false, conc=true, bvars=null) {
+    return this.lhs.evaluate(evaluator, initial, conc, bvars) -
+           this.rhs.evaluate(evaluator, initial, conc, bvars)
+  }
+}
+
+export class RelationalLTE extends BinaryOperator {
+  constructor(lhs, rhs) {
+    super(lhs, rhs)
+  }
+
+  evaluate(evaluator, initial=false, conc=true, bvars=null) {
+    return bumpZero(
+           this.lhs.evaluate(evaluator, initial, conc, bvars) -
+           this.rhs.evaluate(evaluator, initial, conc, bvars))
   }
 }
